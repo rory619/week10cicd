40c54e20541673e680bed7b5f89ce2e6d9db3c98
Rory O Loughlin <g00422319@atu.ie>
2024-12-28 16:40:53 +0000

    committing initial files for week10 lab

---
+++ src/main/java/ie/atu/week10cicd/Week10cicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week10cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10cicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10cicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week10cicd/Week10cicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week10cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10cicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
